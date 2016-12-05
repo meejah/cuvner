074e66863bb022fbc14c431ff8c36e52670416cc
meejah <meejah@meejah.ca>
2016-12-05 13:21:52 -0700

    more feeetures for cuv-watch

--- cuv/watch.py
+++ cuv/watch.py
@@ -28,6 +28,38 @@ from .analysis import CoverageAnalysis, create_analysis
 from .diff import diff_coverage_data
 
 
+def show_missing(data, file_coverage, common):
+    max_fname = max([len(nm) - common for nm in file_coverage])
+    format_str = u'{:>%d}: {}' % (max_fname,)
+    width = click.get_terminal_size()[0]
+
+    for fname in file_coverage:
+        analysis = create_analysis(data, fname)
+        if len(analysis.missing):
+            print(format_str.format(fname[common:], analysis._missing_formatted))
+
+def _new_covered_lines(data_a, data_b, cfg):
+    """
+    """
+    files_a = set(data_a.data.measured_files())
+    files_b = set(data_b.data.measured_files())
+    common_files = files_a.intersection(files_b)
+
+    new_coverage = {}
+    for fname in common_files:
+        a = create_analysis(data_a, fname)
+        b = create_analysis(data_b, fname)
+        new_covered_lines = []
+        if a.statements == b.statements:
+            for x in a.statements:
+                if x in a.missing:
+                    if x not in b.missing:
+                        new_covered_lines.append(x)
+            if new_covered_lines:
+                new_coverage[fname] = new_covered_lines
+    return new_coverage
+
+
 def watch_coverage(keywords, cfg):
     file_coverage = []
 
@@ -51,13 +83,21 @@ def watch_coverage(keywords, cfg):
                 new_data = coverage.Coverage(data_file=coverage_fname)
                 new_data.load()
                 diff_coverage_data(self._existing_data, new_data, cfg)
+                newly_covered = _new_covered_lines(self._existing_data, new_data, cfg)
                 self._existing_data = new_data
+                click.echo('----')
+                click.echo("newly covered:")
+                for k, v in newly_covered.items():
+                    click.echo("  {}: {}".format(k, v))
+                click.echo('----')
+                show_missing(self._existing_data, file_coverage, len(common))
 
     handler = Handler()
     observer = Observer()
     print("Watching: {}".format(coverage_fname))
     observer.schedule(handler, split(coverage_fname)[0])
     observer.start()
+    show_missing(existing_data, file_coverage, len(common))
     try:
         while True:
             sleep(1)
