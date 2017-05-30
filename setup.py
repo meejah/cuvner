925b3aaed421cf4cb49e97c5277b74a58bf7ccad
meejah <meejah@meejah.ca>
2017-05-30 14:23:01 -0600

    pin to ansicolors 1.0.2 as per #7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=[
         'pygments',
         'click',
-        'ansicolors',
+        'ansicolors==1.0.2',
         'coverage',
         'six',
         'incremental',
